8d6f924a990003f060161f733e3e17a89eb8efa4
Ryan Weaver <ryan@thatsquality.com>
2018-12-13 22:16:29 -0500

    tuts hero working...

--- tutorial/src/Entity/Security.php
+++ tutorial/src/Entity/Security.php
@@ -4,24 +4,14 @@ namespace App\Entity;
 
 class Security
 {
-    /**
-     * @var string
-     */
     private $name;
 
-    /**
-     * @var bool
-     */
     private $isActive;
 
-    /**
-     * @var Enclosure
-     */
     private $enclosure;
 
     public function __construct(string $name, bool $isActive, Enclosure $enclosure)
     {
-
         $this->name = $name;
         $this->isActive = $isActive;
         $this->enclosure = $enclosure;
